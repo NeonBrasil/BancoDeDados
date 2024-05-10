4d27606c9ff841af3249132fa1206b1c1ef3e9da
Arthur Sousa <sousaarthur840@gmail.com>
2024-05-09 21:28:42 -0300

    Adicionado executor de arquivo

---
+++ main.js
@@ -0,0 +1,35 @@
+class main {
+    
+    limparBanco() {
+        //Apagar todas as 
+    }
+
+    adicionarTabelas() {
+        //Adicionar as tabelas necessárias
+    }
+
+    popularTabelas() {
+
+    }
+
+    buscarHistoricoEscolar() {
+
+    }
+
+    buscarHistóricoDeDisciplinas() {
+
+    }
+
+    listarFormandos() {
+
+    }
+
+    listarChefesDeDepartamento() {
+
+    }
+
+    buscarGrupoTCC() {
+
+    }
+}
+
