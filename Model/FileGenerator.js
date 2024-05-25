bdce023e045b0251c0196a950410a02b3e5dc76c
Arthur Sousa <sousaarthur840@gmail.com>
2024-05-24 22:58:36 -0300

    File generator

---
+++ Model/FileGenerator.js
@@ -0,0 +1,7 @@
+class FileGenerator {
+    constructor() {}
+
+    transformStringToFile() {
+        
+    }
+}
---
+++ Queries/DeleteTables.sql
@@ -0,0 +1,9 @@
+DROP TABLE defaultdb.matricula
+DROP TABLE defaultdb.tcc
+DROP TABLE defaultdb.aluno
+DROP TABLE defaultdb.matrizcurricular
+DROP TABLE defaultdb.disciplinaministrada
+DROP TABLE defaultdb.disciplina
+DROP TABLE defaultdb.curso
+DROP TABLE defaultdb.departamento
+DROP TABLE defaultdb.professor
